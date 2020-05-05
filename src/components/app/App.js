f0f7cd2318097b511b263f84d05ec36bb0bc3053
Jakob Persson <jakobpersson@Jakobs-MBP.lan>
2020-05-05 14:13:29 +0200

    changed color on navigation bar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -14,7 +14,6 @@ const App = () => {
       <div className="app">
         <Navigation />
         <Switch>
-
           <Route exact path="/" component={Home} />
           <Route path="/courses" component={Courses}/>
           <Route path="/projects" component={Projects}/>
