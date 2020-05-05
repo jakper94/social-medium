83f8f3075908999d42978802b8129e5ac7a0c782
Sara Dimov Stenström <id16sdm@cs.umu.se>
2020-05-05 14:35:52 +0200

    added assignments component

---
+++ src/components/assignments/assignments.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assignments.scss";
+
+const Assignments = () => {
+  return (
+    <section>
+      <div>
+        <h1>Assignments</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Assignments;
