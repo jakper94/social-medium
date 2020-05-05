7719673e0e07bfd80b4a52d1baf5f091d7894f75
Jakob Persson <jakobpersson@Jakobs-MBP.lan>
2020-05-05 14:56:53 +0200

    added right sidebar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,23 +2,26 @@ import React from "react";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navigation from "../navigation/navigation";
 import Home from "../home/home";
-import Courses from "../courses/courses"
-import Projects from "../projeccts/projects"
+import Courses from "../courses/courses";
+import Projects from "../projeccts/projects";
+import RightSideBar from "../right-side-bar/right-side-bar";
 import "./app.scss";
 import "../../resources/site.scss";
-
 
 const App = () => {
   return (
     <HashRouter>
       <div className="app">
+      <RightSideBar />
         <Navigation />
+       
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/courses" component={Courses}/>
-          <Route path="/projects" component={Projects}/>
+          <Route path="/courses" component={Courses} />
+          <Route path="/projects" component={Projects} />
           <Route from="*" to="/" component={Home} />
         </Switch>
+        
       </div>
     </HashRouter>
   );
---
+++ src/components/right-side-bar/right-side-bar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./right-side-bar.scss";
+
+const RightSideBar = () => {
+  return <div className="bar">
+      <h2>Calender</h2>
+      <h2>Deadlines</h2>
+  </div>;
+};
+
+export default RightSideBar;
