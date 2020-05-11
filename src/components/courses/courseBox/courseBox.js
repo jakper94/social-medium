80916c4f9a632edefa73593aa99c6da0fa1d7647
Jakob Persson <jakobpersson@Jakobs-MBP.lan>
2020-05-11 16:15:13 +0200

    fixed in course box

--- src/components/courses/courseBox/courseBox.js
+++ src/components/courses/courseBox/courseBox.js
@@ -42,21 +42,26 @@ const CourseBox = ({
 
   return (
     <a href={"#/course/" + id} className="courseBox">
-      <div className="imageBox">
-        <img src={process.env.PUBLIC_URL + imgUrl} alt={courseName} />
-        {checkIfNewCourse()}
+      <div className="box">
+        <div className="imageBox">
+          <img src={process.env.PUBLIC_URL + imgUrl} alt={courseName} />
+          {checkIfNewCourse()}
+        </div>
+        <h2>{courseName}</h2>
+        <div className="hp">
+          <FaGraduationCap className="graduationCap" />
+          <h4>{hp}hp</h4>
+        </div>
+        <p>{info}</p>
+        <div className="participants">
+          <IoIosPeople className="peopleicon" />{" "}
+          <h4>{participants} participants</h4>
+        </div>
+        <div className="progressBar">{setProgressbar(inputProgress)}</div>
       </div>
-      <h2>{courseName}</h2>
-      <div className="hp">
-        <FaGraduationCap className="graduationCap" />
-        <h4>{hp}hp</h4>
+      <div className="open">
+        <h2>Click to open</h2>
       </div>
-      <p>{info}</p>
-      <div className="participants">
-        <IoIosPeople className="peopleicon" />{" "}
-        <h4>{participants} participants</h4>
-      </div>
-      <div className="progressBar">{setProgressbar(inputProgress)}</div>
     </a>
   );
 };
